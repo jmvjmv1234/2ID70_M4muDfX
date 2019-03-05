8d979087e5d6b05e266988cf5647edd0280264d6
jmvjmv1234 <47352670+jmvjmv1234@users.noreply.github.com>
2019-03-05 16:23:38 +0100

    Update q2Create.sql

--- q2Create.sql
+++ q2Create.sql
@@ -1,1 +1,17 @@
+-- Exercise 5
+CREATE VIEW CourseRegistrationsWithGrade as(
+SELECT * FROM CourseRegistrations as CR WHERE CR.Grade > 0);
 
+-- Exercise 8
+
+CREATE VIEW SACountPerCO as(
+SELECT CO.CourseOfferId, COUNT(DISTINCT SA.StudentRegistrationId) as Count
+FROM CourseOffers as CO, StudentAssistants as SA
+WHERE CO.CourseOfferId = SA.CourseOfferId
+GROUP BY CO.CourseOfferId);
+
+CREATE VIEW SCountPerCO as(
+SELECT CO.CourseOfferId, COUNT(DISTINCT CR.StudentRegistrationId) as Count
+FROM CourseOffers as CO, CourseRegistrations as CR
+WHERE CO.CourseOfferId = CR.CourseOfferId
+GROUP BY CO.CourseOfferId);
